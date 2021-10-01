507bb50e9f3015b48f5c1775441b2a4868b99548
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2021-10-01 12:35:07 +0530

    Added Pattern 1 & 2 programs
    Added Pattern 1 & 2 programs

---
+++ Core_Java/Core_Java_Programs/src/com/sid/pattern/Pattern1.java
@@ -0,0 +1,31 @@
+/**
+ * 	Output:	
+ * 	
+	*****
+	*****
+	*****
+	*****
+	*****
+ */
+
+package com.sid.pattern;
+
+/**
+ * @author Siddhant Patni
+ *
+ */
+public class Pattern1 {
+
+	public static void main(String[] args) 
+	{
+		for (int i = 1; i <= 5; i++) 
+		{
+			for (int j = 1; j <= 5; j++) 
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ Core_Java/Core_Java_Programs/src/com/sid/pattern/Pattern2.java
@@ -0,0 +1,31 @@
+/**
+ * 	Output:		
+ * 
+	11111
+	22222
+	33333
+	44444
+	55555
+ */
+
+package com.sid.pattern;
+
+/**
+ * @author Siddhant Patni
+ *
+ */
+public class Pattern2 {
+
+	public static void main(String[] args) 
+	{
+		for (int i = 1; i <= 5; i++) 
+		{
+			for (int j = 1; j <= 5; j++) 
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
