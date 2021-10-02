d29a1b767524a3c2894be6cb3829785d8fd0babd
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2021-10-02 16:04:54 +0530

    Added find output code
    Added find output code

---
+++ Core_Java/Core_Java_Challenging_Questions/src/com/sid/find_output_programs/FindOutput2.java
@@ -0,0 +1,28 @@
+package com.sid.find_output_programs;
+
+/**
+ * @author Siddhant Patni
+ *
+ */
+public class FindOutput2 
+{
+
+	public static void main(String[] args) 
+	{
+		new FindOutput2();
+	}
+	public void FindOutput2(int z) 
+	{
+		System.out.print(" "+(z*2));
+	}
+	public void FindOutput2(long z) 
+	{
+		System.out.print(" "+z);
+	}
+	public void FindOutput2() 
+	{
+		System.out.print("nothing");
+	}
+}
+
+// Compilation is successful but no output is produced.
---
+++ Core_Java/Core_Java_Challenging_Questions/src/com/sid/find_output_programs/FindOutput3.java
@@ -0,0 +1,28 @@
+package com.sid.find_output_programs;
+
+/**
+ * @author Siddhant Patni
+ *
+ */
+class FindOutput3 
+{
+	public void disp (char c)
+	{
+		System.out.println(c);
+	}
+	public void disp (int c)
+	{
+		System.out.println(c);
+	}
+//}
+//public class Sample
+//{
+	public static void main(String args[])
+	{
+		FindOutput3 obj = new FindOutput3();
+		obj.disp('a');
+		obj.disp (5);
+	}
+}
+
+// Remove commented code for actual problem, I have commented code to make it error free
