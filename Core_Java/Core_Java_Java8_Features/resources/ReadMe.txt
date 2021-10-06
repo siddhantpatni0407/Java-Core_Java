28b966d032cd78346338eeca1606a2ba8935dd85
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2021-10-06 16:20:53 +0530

    Added MaxNumberUsingStream program
    Added MaxNumberUsingStream program

---
+++ Core_Java/Core_Java_Java8_Features/resources/ReadMe.txt
@@ -0,0 +1,6 @@
+Project - Core_Java_Java8_Features
+============================================
+# Package : com.sid.java8_features
+-------------------------------------
+1.	MaxNumberUsingStream.java
+=>	Problem Statement - Find the largest number using java 8 stream 
---
+++ Core_Java/Core_Java_Java8_Features/src/com/sid/java8_features/MaxNumberUsingStream.java
@@ -0,0 +1,16 @@
+/**
+ * 	Problem statement - Find the largest number using java 8 stream 
+ */
+package com.sid.java8_features;
+
+import java.util.Arrays;
+
+public class MaxNumberUsingStream 
+{
+	public static void main(String[] args) 
+	{
+		int num=Arrays.stream(new int[] {3,6,9,1,2}).max().getAsInt();
+		
+		System.out.println("Largest number : "+num);
+	}
+}
