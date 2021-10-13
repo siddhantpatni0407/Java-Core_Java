e899f19e93f7e971b1c0b0083fe684601dabd43d
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2021-10-13 12:38:43 +0530

    StringOperations Program
    StringOperations Program

---
+++ Core_Java/Core_Java_Challenging_Questions/src/com/sid/string_operations/StringOperations.java
@@ -0,0 +1,20 @@
+/**
+ *  Problem statement - Perform various string operations
+ */
+package com.sid.string_operations;
+
+public class StringOperations 
+{
+	public static void main(String[] args) 
+	{
+		String str1="abc";  			
+		String str2=new String("abc"); 	
+		System.out.println(str1.concat("def")); 
+		System.out.println(str2.concat("def")); 
+		System.out.println(str1.concat("def").hashCode()); 
+		System.out.println(str2.concat("def").hashCode()); 
+		System.out.println(str1.hashCode());
+		System.out.println(str2.hashCode());
+		System.out.println(str1.equals(str2));
+	}
+}
