a8f6dcac9c4a58568d73eaeae093e0a3cc71677b
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2021-10-13 12:59:55 +0530

    SumOfPrimeNumbers Program
    SumOfPrimeNumbers Program

---
+++ Core_Java/Core_Java_Challenging_Questions/src/com/sid/tricky_programs/SumOfPrimeNumbers.java
@@ -0,0 +1,39 @@
+/**
+ *	Problem statement - Write a program to return sum of prime numbers  
+ */
+package com.sid.tricky_programs;
+
+/**
+ * @author Siddhant Patni
+ *
+ */
+public class SumOfPrimeNumbers 
+{
+	public static int returnSumOfPrimeNumbers(int inputFromNumber,int inputToNumber) 
+	{
+		int count, sumOfPrimeNumbers = 0;  
+		for(int number = inputFromNumber; number <= inputToNumber; number++)  
+		{  
+			count = 0;  
+			for (int i = 2; i <= number/2; i++)  
+			{  
+				if(number % i == 0)  
+				{  
+					count++;  
+					break;  
+				} 
+			}  
+			if(count == 0 && number != 1 )  
+			{  
+				sumOfPrimeNumbers = sumOfPrimeNumbers + number;  
+			}  
+		}  
+		return sumOfPrimeNumbers;
+	}
+	public static void main(String args[])   
+	{  
+		int inputFromNumber = 1;
+		int inputToNumber = 10;
+		System.out.println("The Sum of Prime Numbers from " +inputFromNumber+ " to "+inputToNumber+" is : " + returnSumOfPrimeNumbers(inputFromNumber,inputToNumber));  
+	}   
+}
