063e4ed4f7244e2a291a9eb5f37271fe759b4a57
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2021-10-28 17:14:18 +0530

    PrimeFactors Program
    Program to get prime factors for given number

---
+++ Core_Java/Core_Java_Programs/src/com/sid/basic_programs/PrimeFactors.java
@@ -0,0 +1,28 @@
+/**
+ * 	Problem statement - Write a program to print prime factors for given number
+ */
+package com.sid.basic_programs;
+
+public class PrimeFactors 
+{
+	public static void getPrimeFactors(int number) 
+	{
+		for(int i=2;i<number;i++) 
+		{
+			while(number % 2 == 0) 
+			{
+				System.out.println(i+" ");
+				number = number/2;
+			}
+			if(number > 2) 
+			{
+				System.out.println(number);
+			}
+		}
+	}
+	public static void main(String[] args)
+	{
+		int number = 24;
+		getPrimeFactors(number);
+	}
+}
