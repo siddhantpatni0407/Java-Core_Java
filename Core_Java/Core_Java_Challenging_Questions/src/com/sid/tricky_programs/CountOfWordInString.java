86ed79ff6a5d8ee2aa42ebe81f95d9806be42f8f
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2021-12-01 13:05:53 +0530

    Updated CountOfWordInString Program
    Added java 8 method to get count of words in string

--- Core_Java/Core_Java_Challenging_Questions/src/com/sid/tricky_programs/CountOfWordInString.java
+++ Core_Java/Core_Java_Challenging_Questions/src/com/sid/tricky_programs/CountOfWordInString.java
@@ -3,9 +3,17 @@
  */
 package com.sid.tricky_programs;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+/**
+ * @author Siddhant Patni
+ *
+ */
 public class CountOfWordInString 
 {
 	public static void getCountOfWordUsingHashmap(String str) 
@@ -21,7 +29,7 @@ public class CountOfWordInString
             if (integer == null)
             {
             	hashMap.put(word, 1);
-            }
+            }	
             else 
             {
                 hashMap.put(word, integer + 1);
@@ -30,9 +38,20 @@ public class CountOfWordInString
         //System.out.println(hashMap);
         hashMap.forEach((key, value) -> System.out.println(key + " = " + value));
 	}
+	
+	public static void getCountOfWordUsingStream(String str) 
+	{
+		List<String> list = Arrays.asList(str.split(" "));
+		Map<String, Long> map = list.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		map.forEach((key,val) -> System.out.println(key+ " - "+val));
+	}
+	
 	public static void main(String[] args) 
 	{
 		String str = "This is a string This is statement";
+		System.out.println("Method - 1");
 		getCountOfWordUsingHashmap(str);
+		System.out.println("Method - 2");
+		getCountOfWordUsingStream(str);
 	}
 }
