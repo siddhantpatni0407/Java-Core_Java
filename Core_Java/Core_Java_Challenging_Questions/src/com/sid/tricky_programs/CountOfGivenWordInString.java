5d1b32300f1f307ad9d4eceef74f3c7d98e5c67c
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2021-12-01 09:24:48 +0530

    Updated CountOfGivenWordInString Program
    Updated CountOfGivenWordInString Program

--- Core_Java/Core_Java_Challenging_Questions/src/com/sid/tricky_programs/CountOfGivenWordInString.java
+++ Core_Java/Core_Java_Challenging_Questions/src/com/sid/tricky_programs/CountOfGivenWordInString.java
@@ -3,26 +3,30 @@
  */
 package com.sid.tricky_programs;
 
+/**
+ * @author Siddhant Patni
+ *
+ */
 public class CountOfGivenWordInString 
 {
-	public static void getCountOfGivenWord(String str, String word)
+	public static int getCountOfGivenWord(String str, String word)
 	{
-	    // split the string by spaces in a
 	    String a[] = str.split(" ");
-	 
 	    int count = 0;
 	    for (int i = 0; i < a.length; i++)
 	    {
-	    if (word.equals(a[i]))
-	        count++;
+	    	if (word.equals(a[i])) 
+	    	{
+	    		count++;
+	    	}
 	    }
-	    System.out.println("Count of "+word+" : "+count);
+	    return count;
 	}
 	
 	public static void main(String[] args) 
 	{
 		String str = "This is a string This is statement";
 		String word ="This";
-		getCountOfGivenWord(str,word);
+		 System.out.println("Count of word "+word + " : " +getCountOfGivenWord(str,word));
 	}
 }
